2cd458edfb1734490d22e83e8f42d997ce42a396
RAZELABS <dev@raze.pe>
2024-11-28 11:31:44 -0500

    zeus 20241128

--- resources/views/admin/configuraciones/index.blade.php
+++ resources/views/admin/configuraciones/index.blade.php
@@ -1,26 +1,60 @@
 @extends('admin.layouts.app')
 @section('content')
-    <div class="container p-3">
-        <div class="row mt-3">
-            <div class="col-12 col-md-12 px-3">
-                <div class="row bg-configuracion b-radius-20 px-2 mt-3 mt-md-0">
-                    <div class="col-12 px-4 pt-2 text-center font-weight-bolder text-secondary">Información General</div>
-
-                    {{-- parametros generales --}}
-                    <div class="col-6 col-md-3">
-                        <div class="featured-boxes  m-0 ">
-                            <div class="featured-box my-2 b-radius-20 ">
-                                <a href="{{ route('admin.parametros.index') }}" class="text-decoration-none">
-                                    <span class="p-1 text-center d-block">
-                                        <span class="text-secondary text-xl"><i class="fas fa-file-circle-check"></i></span>
-                                        <span class="font-weight-bold    d-block text-secondary">Parametros</span>
-                                    </span>
-                                </a>
-                            </div>
+<div class="container p-3">
+    <div class="row mt-3">
+        <div class="col-12 px-3">
+            <div class="row bg-configuracion b-radius-20 px-2 mt-3 mt-md-0">
+                <div class="col-12 px-4 pt-2 text-center font-weight-bolder text-secondary">matriz
+                </div>
+                <div class="col-3">
+                    <div class="featured-boxes  m-0  ">
+                        <div class="featured-box my-2 b-radius-20 ">
+                            <a href="{{ url('p_carbon') }}" class="text-decoration-none">
+                                <span class="p-1 text-center d-block">
+                                    <span class="text-secondary display-5"><i class="fa-solid fa-mound"></i></span>
+                                    <span class="font-weight-bold    d-block text-secondary ">Carbon</span>
+                                </span>
+                            </a>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-3">
+                    <div class="featured-boxes  m-0  ">
+                        <div class="featured-box my-2 b-radius-20 ">
+                            <a href="{{ url('p_carbon') }}" class="text-decoration-none">
+                                <span class="p-1 text-center d-block">
+                                    <span class="text-secondary display-5"><i class="fa-solid fa-mound"></i></span>
+                                    <span class="font-weight-bold    d-block text-secondary ">Carbon</span>
+                                </span>
+                            </a>
                         </div>
                     </div>
                 </div>
             </div>
         </div>
     </div>
+</div>
+<div class="container p-3">
+    <div class="row mt-3">
+        <div class="col-12 col-md-12 px-3">
+            <div class="row bg-configuracion b-radius-20 px-2 mt-3 mt-md-0">
+                <div class="col-12 px-4 pt-2 text-center font-weight-bolder text-secondary">Información General</div>
+
+                {{-- parametros generales --}}
+                <div class="col-6 col-md-3">
+                    <div class="featured-boxes  m-0 ">
+                        <div class="featured-box my-2 b-radius-20 ">
+                            <a href="{{ route('admin.parametros.index') }}" class="text-decoration-none">
+                                <span class="p-1 text-center d-block">
+                                    <span class="text-secondary display-5"><i class="fas fa-file-circle-check"></i></span>
+                                    <span class="font-weight-bold    d-block text-secondary">Parametros</span>
+                                </span>
+                            </a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
 @endsection
--- resources/views/frontend/layouts/footer.blade.php
+++ resources/views/frontend/layouts/footer.blade.php
@@ -1,69 +1,13 @@
 <footer id="footer" class="m-0 border-0 bg-color-quaternary overflow-hidden">
     <div class="container">
-        <div class="row py-5 custom-row-footer">
-            <div
-                class="col-12 col-sm-12 col-lg-3 d-flex align-items-start flex-column footer-column custom-footer-column-logo">
-                <img width="123" src="{{asset('img/logo-3.png')}}" alt="Logo Footer">
-                <p class="d-block m-0 text-color-default">Feel free to reach out to us through any of these ways.</p>
-            </div>
-            <div class="col-12 col-sm-12 col-lg-9 footer-column">
+        <div class="row py-1 custom-row-footer">
+            
+            <div class="col-12  footer-column">
                 <div
                     class="d-flex align-items-start align-sm-items-end justify-content-between flex-column h-100 mt-4 mt-sm-0">
-                    <div
-                        class="d-flex flex-nowrap flex-lg-wrap justify-content-start justify-content-lg-end align-items-start align-items-lg-center w-100 flex-column flex-lg-row mt-4 mt-lg-0 custom-container-info-socials">
-                        <ul class="nav nav-pills justify-content-between h-100 mb-4 mb-lg-0">
-                            <li class="nav-item d-inline-flex flex-column flex-lg-row">
-                                <span
-                                    class="footer-nav-phone py-2 d-flex align-items-center text-color-secondary font-weight-semibold text-uppercase justify-content-start mb-2 mb-lg-0">
-                                    <span>
-                                        <img width="15" height="18" src="{{ asset('frontend/img/icons/phone.svg') }}"
-                                            alt="Phone">
-                                    </span>
-                                    <a class="font-weight-bold text-color-secondary text-color-hover-primary text-decoration-none"
-                                        href="tel:123-456-7890">(+1) 214-997-4331</a>
-                                </span>
-                                <span
-                                    class="footer-nav-email px-0 font-weight-normal d-flex align-items-center justify-content-start mb-2 mb-lg-0">
-                                    <span>
-                                        <img width="25" height="18" src="{{ asset('frontend/img/icons/mail.svg') }}"
-                                            alt="Mail">
-                                    </span>
-                                    <a class="text-color-secondary text-color-hover-primary text-decoration-none"
-                                        href="mailto:Info@z-cconsultants.com">Info@z-cconsultants.com</a>
-                                </span>
-                                <span
-                                    class="footer-nav-opening-hours px-0 font-weight-normal d-flex align-items-center text-color-secondary justify-content-start mb-2 mb-lg-0">
-                                    <span>
-                                        <img width="19" height="18" src="{{ asset('frontend/img/icons/calendar.svg') }}"
-                                            alt="Calendar">
-                                    </span>
-                                    Mon - Sat 9:00am - 6:00pm / Sunday - CLOSED
-                                </span>
-                            </li>
-                        </ul>
-                        <ul class="social-icons custom-social-icons">
-                            <li class="social-icons-instagram">
-                                <a class="custom-bg-color-light-grey" href="http://www.instagram.com/" target="_blank"
-                                    title="Instagram">
-                                    <i class="fab fa-instagram text-4 font-weight-semibold text-color-secondary"></i>
-                                </a>
-                            </li>
-                            <li class="social-icons-twitter">
-                                <a class="custom-bg-color-light-grey" href="http://www.twitter.com/" target="_blank"
-                                    title="Twitter">
-                                    <i class="fab fa-x-twitter text-4 font-weight-semibold text-color-secondary"></i>
-                                </a>
-                            </li>
-                            <li class="social-icons-facebook">
-                                <a class="custom-bg-color-light-grey" href="http://www.facebook.com/" target="_blank"
-                                    title="Facebook">
-                                    <i class="fab fa-facebook-f text-4 font-weight-semibold text-color-secondary"></i>
-                                </a>
-                            </li>
-                        </ul>
-                    </div>
+                    
                     <nav class="nav-footer w-100 d-none d-lg-block">
-                        <ul class="nav nav-pills justify-content-end" id="mainNav">
+                        <ul class="nav nav-pills justify-content-center" id="mainNav">
                             <li class="dropdown-secondary">
                                 <a class="nav-link text-color-secondary font-weight-bold letter-spacing-05 text-color-hover-primary"
                                     href="#">
@@ -92,7 +36,7 @@
     <div class="footer-copyright container bg-color-quaternary">
         <div class="row">
             <div class="col-lg-12 text-center m-0">
-                <p class="text-color-grey">ZyC Consultants. © 2024. All Rights Reserved</p>
+                <p class="text-color-grey">© TODOS LOS DERECHOS RESERVADOS, Falabella.com S.A.C. Av. Paseo de la República 3220, San Isidro, Perú</p>
             </div>
         </div>
     </div>
